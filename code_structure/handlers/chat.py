5d0ff040c5a81daa0a149c3f85c8e7338805bbef
Dias <kasenov_06@internet.ru>
2026-07-07 20:23:55 +0500

    added checkings and fixed some bugs

--- code_structure/handlers/chat.py
+++ code_structure/handlers/chat.py
@@ -1,21 +1,34 @@
 from aiogram import Router
 from aiogram.types import Message
 from code_structure.state.states import user_mode
-from code_structure.services.ai import client, ask_ai, send_long_messages
-
+from code_structure.services.ai import ask_ai, send_long_messages
+from code_structure.state.memory import (
+    get_history,
+    add_user_message,
+    add_ai_message
+)
 chat_router = Router()
 
 @chat_router.message()
 async def ai_chat(message: Message):
     mode = user_mode.get(message.from_user.id)
 
-    if message.from_user.id not in user_mode:
+    if mode is None:
         await message.answer("Выбери режим 👇")
         return
 
     if mode == "chat":
 
-        response = await ask_ai(message.text)
+        user_id = message.from_user.id
+
+        add_user_message(user_id, message.text)
+
+        history = get_history(user_id)
+
+        response = await ask_ai(history)
+
+        add_ai_message(user_id, response)
+
         await send_long_messages(message, response)
     elif mode == "text":
         prompt = ("Ты профессиональный помощник по написанию текстов.\n"
--- code_structure/services/ai.py
+++ code_structure/services/ai.py
@@ -1,4 +1,5 @@
 from google import genai
+from google.genai.errors import ServerError
 from dotenv import load_dotenv
 from os import getenv
 from aiogram.types import Message
@@ -6,15 +7,46 @@ from aiogram.types import Message
 load_dotenv()
 client = genai.Client(api_key=getenv("AI_API"))
 
-async def ask_ai(prompt: str) -> str:
-    response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents=prompt
-    )
-    return response.text
+async def ask_ai(history):
+    if isinstance(history, str):
+        contents = [
+            {
+                "role": "user",
+                "parts": [
+                    {"text": history}
+                ]
+            }
+        ]
 
+    else:
+        contents = []
+
+        for message in history:
+            contents.append({
+                "role": message["role"],
+                "parts": [
+                    {"text": message["text"]}
+                ]
+            })
+
+    try:
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=contents
+        )
+
+        return response.text
+
+    except ServerError:
+        return "⚠️ AI сейчас перегружен. Попробуйте ещё раз через несколько секунд."
 async def send_long_messages(message: Message, text: str):
     max_length = 4096
+
+    text = text.replace("**", "")
+    text = text.replace("##", "")
+    text = text.replace("```", "")
+    text = text.replace("---", "")
+    text = text.replace("#", "")
 
     for i in range(0, len(text), max_length):
         await message.answer(text[i:i + max_length])
